0b7f4e8cb1f3e2485c0a71e3a820c82bb87d3750
kyohei <kyohenoki@gmail.com>
2025-10-18 21:57:39 +0900

    slug 参考 h

---
+++ src/content/config.ts
@@ -0,0 +1,15 @@
+import { defineCollection, z } from 'astro:content'
+import { glob } from 'astro/loaders'
+
+const docs = defineCollection({
+  loader: glob({ pattern: '**/**/*.md', base: './src/content/docs' }),
+  schema: z.object({
+    number: z.number(),
+    title: z.string(),
+    description: z.string(),
+    date: z.string(),
+    update: z.string(),
+  }),
+})
+
+export const collections = { docs }
---
+++ src/content/docs/copyparty.md
@@ -0,0 +1,11 @@
+---
+number: 1
+title: はじめに
+description: copyparty について
+date: 2025/10/18 21:11
+update: none
+---
+
+copyparty は、ポータブルなファイルサーバーです
+
+ノルウェーのプログラマである ed 氏によって5年の歳月をかけて制作され、現在も開発中である
